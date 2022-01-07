18ad49e546475c5bb1ad933562c1e9a88f104a84
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-01-07 19:18:00 +0100

    adding a product component

--- client/components/Products/Item/index.tsx
+++ client/components/Products/Item/index.tsx
@@ -1,5 +1,17 @@
-const Product = () => {
-  return <div></div>;
+import Link from 'next/link';
+import { Card } from 'react-bootstrap';
+import { ProductInterface } from '../../../interfaces';
+
+interface ProductProps {
+  product: ProductInterface;
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link href={`/product/${product._id}`}></Link>
+    </Card>
+  );
 };
 
 export default Product;
---
+++ client/interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface ProductInterface {
+  _id: string;
+  name: string;
+  image: string;
+  description: string;
+  brand: string;
+  category: string;
+  price: number;
+  countInStock: number;
+  rating: number;
+  numReviews: number;
+}
