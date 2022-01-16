b659980bb99212fe6bda2beadc5253d302e2bf1e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-01-16 03:08:34 +0100

    adding a message and loader components

---
+++ client/components/Message/index.tsx
@@ -0,0 +1,10 @@
+import { Alert } from 'react-bootstrap';
+
+const Message: React.FC<{ variant?: string }> = ({
+  variant = 'info',
+  children,
+}) => {
+  return <Alert variant={variant}> {children}</Alert>;
+};
+
+export default Message;
