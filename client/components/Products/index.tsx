44700564fc9d66ef4f0237f6ac7e80de85acd815
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-01-16 03:01:50 +0100

    successfully fetching products

--- client/components/Products/index.tsx
+++ client/components/Products/index.tsx
@@ -1,24 +1,37 @@
 //importing types & utils
 import { v4 as randomID } from 'uuid';
-import { ProductInterface } from '../../interfaces';
 //importing components
 import { Row, Col } from 'react-bootstrap';
 import Item from './Item';
+import { useEffect } from 'react';
+import { useProductsActions } from '../../hooks/useActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 
-interface ProductsProps {
-  products: ProductInterface[];
-}
+const Products: React.FC = () => {
+  const { fetchProducts } = useProductsActions();
+  const { loading, error, products } = useTypedSelector(
+    state => state.products
+  );
 
-const Products: React.FC<ProductsProps> = ({ products }) => {
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
   return (
     <>
       <h1 className="py-2">Latest products</h1>
       <Row>
-        {products.map(product => (
-          <Col sm={12} md={6} lg={4} xl={3} key={randomID()}>
-            <Item {...product} />
-          </Col>
-        ))}
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h3>{error}</h3>
+        ) : (
+          products.map(product => (
+            <Col sm={12} md={6} lg={4} xl={3} key={randomID()}>
+              <Item {...product} />
+            </Col>
+          ))
+        )}
       </Row>
     </>
   );
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,35 +1,19 @@
 //importing types & utils
 import { seoConfig } from '../utils';
-import { productsAPI } from '../lib';
-import { GetStaticProps, NextPage } from 'next';
+import { NextPage } from 'next';
 //importing components
 import SEO from '../components/SEO';
 import Products from '../components/Products';
-import { ProductInterface } from '../interfaces';
 
-interface HomeProps {
-  products: ProductInterface[];
-}
-
-const Home: NextPage<HomeProps> = ({ products }) => {
+const Home: NextPage = () => {
   return (
     <>
       <SEO {...seoConfig} />
       <main className="wrapper py-5">
-        <Products products={products} />
+        <Products />
       </main>
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await productsAPI('/products');
-
-  return {
-    props: {
-      products: data,
-    },
-  };
 };
 
 export default Home;
