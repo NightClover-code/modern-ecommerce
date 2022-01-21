a7e155ee18d5f5320952459219ee21df3bebcda1
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-01-21 16:47:52 +0100

    handling product details errors and loading

--- client/components/ProductDetails/index.tsx
+++ client/components/ProductDetails/index.tsx
@@ -4,6 +4,8 @@ import Rating from '../Rating';
 import { useProductsActions, useTypedSelector } from '../../hooks';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Loader from '../Loader';
+import Message from '../Message';
 
 interface ProductDetailsProps {
   pageId: string | string[] | undefined;
@@ -23,52 +25,60 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ pageId }) => {
   }, [fetchProduct, pageId]);
 
   return (
-    <Row>
-      <Col md={6}>
-        <Image src={image} alt={name} fluid />
-      </Col>
-      <Col md={3}>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <h3>{name}</h3>
-          </ListGroup.Item>
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={image} alt={name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{name}</h3>
+              </ListGroup.Item>
 
-          <ListGroup.Item>
-            <Rating value={rating} text={`${numReviews} reviews`} />
-          </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating value={rating} text={`${numReviews} reviews`} />
+              </ListGroup.Item>
 
-          <ListGroup.Item>Price: ${price}</ListGroup.Item>
-          <ListGroup.Item>Description: {description}</ListGroup.Item>
-        </ListGroup>
-      </Col>
-      <Col md={3}>
-        <Card>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Row>
-                <Col>Price:</Col>
-                <Col>
-                  <strong>${price}</strong>
-                </Col>
-              </Row>
-            </ListGroup.Item>
+              <ListGroup.Item>Price: ${price}</ListGroup.Item>
+              <ListGroup.Item>Description: {description}</ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
 
-            <ListGroup.Item>
-              <Row>
-                <Col>Status:</Col>
-                <Col>{countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
-              </Row>
-            </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>{countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+                  </Row>
+                </ListGroup.Item>
 
-            <ListGroup.Item>
-              <Button className="w-100" type="button">
-                Add To Cart
-              </Button>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
-      </Col>
-    </Row>
+                <ListGroup.Item>
+                  <Button className="w-100" type="button">
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
