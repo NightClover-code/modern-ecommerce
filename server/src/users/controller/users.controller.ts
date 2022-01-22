52cbe2ac8e7b6bd8be056183f039f3a8a03fc567
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-01-22 23:59:28 +0100

    adding user signin functionality

--- server/src/users/controller/users.controller.ts
+++ server/src/users/controller/users.controller.ts
@@ -1,11 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from '../dtos';
+import { AuthService } from '../services/auth.service';
 import { UsersService } from '../services/users.service';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+  ) {}
 
   @Post('/signin')
-  logInUser(@Body() { email, password }: CreateUserDto) {}
+  async logInUser(@Body() { email, password }: CreateUserDto) {
+    const user = await this.authService.signin(email, password);
+
+    return user;
+  }
 }
--- server/src/users/services/auth.service.ts
+++ server/src/users/services/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async signin(email: string, password: string) {}
+  async signin(email: string, password: string) {
+    const user = await this.usersService.findOne(email);
+
+    if (!user) throw new NotFoundException('user not found');
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) throw new BadRequestException('Invalid password');
+
+    return user;
+  }
 }
