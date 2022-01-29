0d0eb6c5bbfecfaae0611d97d3342f220307df10
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-01-29 16:55:34 +0100

    updating cart actions v2

--- client/state/Cart/cart.actions.ts
+++ client/state/Cart/cart.actions.ts
@@ -1,16 +1,40 @@
 import { CartItemInterface } from '../../interfaces';
 import { ActionTypes } from './cart.action-types';
 
-export type CartAction = GetCartItems | AddCartItem | RemoveCartItem;
+export type CartAction =
+  | AddCartItemStart
+  | AddCartItemSuccess
+  | AddCartItemError
+  | GetCartItemsStart
+  | GetCartItemsSuccess
+  | GetCartItemsError;
 
-export interface AddCartItem {
-  type: ActionTypes.ADD_CART_ITEM;
+export interface AddCartItemStart {
+  type: ActionTypes.ADD_CART_ITEM_START;
+}
+
+export interface AddCartItemSuccess {
+  type: ActionTypes.ADD_CART_ITEM_SUCCESS;
   payload: CartItemInterface;
 }
 
-export interface GetCartItems {
-  type: ActionTypes.GET_CART_ITEMS;
+export interface AddCartItemError {
+  type: ActionTypes.ADD_CART_ITEM_ERROR;
+  payload: string;
+}
+
+export interface GetCartItemsStart {
+  type: ActionTypes.GET_CART_ITEMS_START;
+}
+
+export interface GetCartItemsSuccess {
+  type: ActionTypes.GET_CART_ITEMS_SUCCESS;
   payload: CartItemInterface[];
+}
+
+export interface GetCartItemsError {
+  type: ActionTypes.GET_CART_ITEMS_ERROR;
+  payload: string;
 }
 
 export interface RemoveCartItem {
