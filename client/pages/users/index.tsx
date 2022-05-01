dc43a03ec1c3775eceff7d2961b2ebfb179056f4
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-05-01 21:03:55 +0000

    mixed: checking in on the project

--- client/pages/users/index.tsx
+++ client/pages/users/index.tsx
@@ -1,7 +1,6 @@
 import { NextPage } from 'next';
 import SEO from '../../components/SEO';
 import UsersList from '../../components/UsersList';
-import { useTypedSelector } from '../../hooks';
 import { seoConfig } from '../../utils';
 
 const UsersPage: NextPage = () => {
