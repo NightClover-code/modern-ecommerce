8dc1e4ce2ac81d22c9d3b4bd682a17ded209d07c
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-06-09 01:03:41 +0100

    server: reimplementing jwt auth

--- server/src/guards/jwt-auth.guard.ts
+++ server/src/guards/jwt-auth.guard.ts
@@ -1,11 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-
-    return request.session.user;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
