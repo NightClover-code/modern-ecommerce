d0df89a6ba85dabe74c4a55cd39fa7c54460d224
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-06-10 14:35:47 +0100

    client: admin products component initial styling

--- client/components/ProductsList/index.tsx
+++ client/components/ProductsList/index.tsx
@@ -1,26 +1,39 @@
-import { Row, Col, Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { Row, Col, Button, Table } from 'react-bootstrap';
+import { useAdmin, useProductsActions, useTypedSelector } from '../../hooks';
 import Loader from '../Loader';
 import Message from '../Message';
 
 const ProductsList = () => {
+  useAdmin();
+
+  const { loading, error, data } = useTypedSelector(state => state.products);
+  const user = useTypedSelector(state => state.user);
+  const { fetchProducts } = useProductsActions();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
   return (
     <>
-      {/* <Row className="align-items-center">
+      <Row className="align-items-center">
         <Col>
           <h1>Products</h1>
         </Col>
         <Col className="text-right">
-          <Button className="my-3" onClick={createProductHandler}>
+          {/* <Button className="my-3" onClick={createProductHandler}>
             <i className="fas fa-plus"></i> Create Product
-          </Button>
+          </Button> */}
         </Col>
       </Row>
 
-      {loadingDelete && <Loader />}
+      {/* {loadingDelete && <Loader />}
       {errorDelete & <Message variant="danger">{errorDelete}</Message>}
 
       {loadingCreate && <Loader />}
-      {errorCreate && <Message variant="danger">{errorCreate}</Message>}
+      {errorCreate && <Message variant="danger">{errorCreate}</Message>} */}
 
       {loading ? (
         <Loader />
@@ -40,7 +53,7 @@ const ProductsList = () => {
               </tr>
             </thead>
             <tbody>
-              {products.map(product => (
+              {data.map(product => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
@@ -48,15 +61,23 @@ const ProductsList = () => {
                   <td>{product.category}</td>
                   <td>{product.brand}</td>
                   <td>
-                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Link href={`/admin/products/edit/${product._id}`} passHref>
                       <Button variant="light" className="btn-sm">
                         <i className="fas fa-edit"></i>
                       </Button>
-                    </LinkContainer>
+                    </Link>
                     <Button
                       variant="danger"
                       className="btn-sm"
-                      onClick={() => deleteHandler(product._id)}
+                      onClick={() => {
+                        if (
+                          window.confirm(
+                            'Are you sure you want to delete this user?'
+                          )
+                        ) {
+                          // deleteUser(_user._id);
+                        }
+                      }}
                     >
                       <i className="fas fa-trash"></i>
                     </Button>
@@ -65,9 +86,9 @@ const ProductsList = () => {
               ))}
             </tbody>
           </Table>
-          <Paginate pages={pages} page={page} isAdmin={true} />
-        </> */}
-      {/* )} */}
+          {/* <Paginate pages={pages} page={page} isAdmin={true} /> */}
+        </>
+      )}
     </>
   );
 };
