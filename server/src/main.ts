1c4c8ccc183f35e972afb30b73311db9c416530c
Achraf Dev <elmouhib2003achraf@gmail.com>
2024-12-07 23:14:40 +0100

    feat (server): dependency cleanup & swiching to pnpm

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,13 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as session from 'express-session';
-import { corsConfig, sessionConfig } from './utils/config';
+import { corsConfig } from './utils/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
-const MongoDBStore = require('connect-mongodb-session')(session);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.set('trust proxy', 1); // trust first proxy
+  app.set('trust proxy', 1);
   app.enableCors(corsConfig());
-  app.use(session(sessionConfig(MongoDBStore)));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 4000);
 }
--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,43 +1,16 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModuleOptions } from '@nestjs/mongoose';
-import { SessionOptions } from 'express-session';
 
 export const connectDB = (
-  configService: ConfigService
-): MongooseModuleOptions => {
-  const dbPassword = configService.get<string>('MONGODB_PASSWORD');
-  const dbName = configService.get<string>('MONGODB_DATABASE_NAME');
-
-  const mongodbUri = `mongodb+srv://achrafdev:${dbPassword}@proshop.rbkt6.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-
-  return {
-    uri: mongodbUri,
-    autoIndex: false,
-  };
-};
+  configService: ConfigService,
+): MongooseModuleOptions => ({
+  uri: configService.get<string>('MONGODB_URI'),
+  autoIndex: true,
+});
 
 export const corsConfig = (): CorsOptions => ({
   origin: process.env.CLIENT_URL,
   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
   credentials: true,
-});
-
-export const sessionConfig = (MongoDBStore: any): SessionOptions => ({
-  secret: process.env.SESSION_KEY,
-  resave: false,
-  saveUninitialized: false,
-  cookie:
-    process.env.NODE_ENV === 'production'
-      ? {
-          httpOnly: true,
-          sameSite: 'none',
-          secure: true,
-          maxAge: 3 * 24 * 60 * 60 * 1000,
-        }
-      : { maxAge: 3 * 24 * 60 * 60 * 1000 },
-  store: new MongoDBStore({
-    uri: process.env.MONGODB_URL,
-    collection: 'sessions',
-  }),
 });
